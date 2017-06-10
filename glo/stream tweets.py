6ea05907681720e871f69bf468c0e43dbdd52c9d
Dansteve <danstevea@gmail.com>
2017-06-10 21:22:31 +0100

    Create stream tweets.py

--- glo/stream tweets.py
+++ glo/stream tweets.py
@@ -7,15 +7,14 @@ import json
 api = twitter.Api(consumer_key='mTjYFcfaSEEpRc8wvq23MYgdV', consumer_secret='IC7q2Uz3p1Uo2VpzsEQJMJg9CKwPCKv90FgVgvoesSC21SU17B', access_token_key='1140329298-qqaNc2XCYTpW6JRjYdT4XBIDDim1G3HbP0ewEMY',access_token_secret='rGr9pSVz8krjWlp4eZKBrI3nw38htSrlsN2eML9zxXVAq')
    
 #stream #gloworld tweet
-glostream = api.GetStreamFilter(track="Google")
+glostream = api.GetStreamFilter(track="glocare")
 
 
 #write to file glo.txt
 for tweet in glostream:
     try:
-        json1 = json.dumps(tweet, ensure_ascii=False)
-    
-        with open('glo.txt', 'a+') as outfile:
+         
+        with open('glo.json', 'a+') as outfile:
             outfile.write(json.dumps(tweet)+ '\n')
             
     except:
