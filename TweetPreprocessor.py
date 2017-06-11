eef3a7efa241443710f29068f523126e24669658
Obande Otse <otseobande@gmailcom>
2017-06-11 23:10:32 +0100

    minor

--- TweetPreprocessor.py
+++ TweetPreprocessor.py
@@ -1,5 +1,12 @@
 import nltk
+import json
+import re
 
 class TweetPreprocessor():
-	def __init__(self):
+	def __init__(self,filepath="glotweets.json"):
+		self.filepath = filepath
+
+		with open("filepath", "r") as tweetfile:
+			self.tweets = json.load(tweetfile)
+
 		
