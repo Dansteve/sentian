e129fcafc9b668825070aec6cb408b1db4e007b1
Obande Otse <otseobande@gmail.com>
2017-06-11 13:14:51 +0100

    Add files via upload
    Proposed pattern

---
+++ SentianTweets.py
@@ -0,0 +1,24 @@
+#This is a proposed pattern i want us to develop on
+
+import twitter
+import nltk
+
+class SentianTweets():
+	def __init__(self):
+		pass
+
+	def collect(self):
+		pass
+
+	def preprocess(self):
+		pass
+
+	def extractFeatures(self):
+		pass
+
+	def train(self):
+		pass
+
+	def classify(self):
+		pass
+
