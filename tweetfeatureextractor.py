ba9f0a48309a2da57fc21def7ea3b8caa89445b2
Obande Otse <otseobande@gmail.com>
2017-06-13 00:25:47 +0100

    minor edit

--- tweetfeatureextractor.py
+++ tweetfeatureextractor.py
@@ -8,7 +8,7 @@ class FeatureExtractor(DictVectorizer):
 		super().__init__()
 		self.tagged_tweets = tagged_tweets
 
-	def pos_vectorized(self):
+	def pos_vectorizer(self):
 		self.featureExtract = super().fit_transform(self.tagged_tweets)
 		return self.featureExtract
 
@@ -16,6 +16,6 @@ if __name__ == '__main__':
     tp = TweetPreprocessor()
     tt = tp.tagged_tweets
     fe = FeatureExtractor(tt)
-    pro = fe.pos_vectorized()
+    pro = fe.pos_vectorizer()
     print(pro)
     
