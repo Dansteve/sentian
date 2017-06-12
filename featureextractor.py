62b0e017527a9998b777a5ce1c2f527b329d246e
Dansteve <danstevea@gmail.com>
2017-06-12 22:54:23 +0100

    Create featureextractor.py

---
+++ featureextractor.py
@@ -0,0 +1,21 @@
+from tweetpreprocessor import TweetPreprocessor
+from sklearn.feature_extraction import DictVectorizer
+
+
+class featureExtraction(DictVectorizer):
+
+	def __init__(self,tag_tp):
+		self.tag_tweets = tag_tp
+
+	def pos_vectorized(self):
+        self.vec = DictVectorizer()
+		self.featureExtract = self.vec.fit_transform(self.tag_tweets)
+		return self.self.featureExtract
+
+if __name__ == '__main__':
+	tp = TweetPreprocessor()
+    tag_tp = tp.tagged_tweets
+    fE = featureExtraction()
+    pro = fE.pos_vectorized(tag_tp)
+    print(pro)
+    
