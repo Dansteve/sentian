40177c1d993b927d632ae9251809b5d76ae7a7cd
Obande Otse <otseobande@gmail.com>
2017-06-13 13:31:05 +0100

    coupled featureExtractor with tweetpreprocessor

--- tweetfeatureextractor.py
+++ tweetfeatureextractor.py
@@ -4,17 +4,16 @@ from sklearn.feature_extraction import DictVectorizer
 
 class FeatureExtractor(DictVectorizer):
 
-	def __init__(self,tagged_tweets):
+	def __init__(self):
 		super().__init__()
-		self.tagged_tweets = tagged_tweets
+		self._tp = TweetPreprocessor()
+		self.tagged_tweets = self._tp.tagged_tweets
 
 	def pos_vectorizer(self):
 		self.featureExtract = super().fit_transform(self.tagged_tweets)
 		return self.featureExtract
 
 if __name__ == '__main__':
-    tp = TweetPreprocessor()
-    tt = tp.tagged_tweets
     fe = FeatureExtractor(tt)
     pro = fe.pos_vectorizer()
     print(pro)
